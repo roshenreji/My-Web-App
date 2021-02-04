da0762740d578b3abe6dc63d96307a9474a519b9
roshenreji <roshenreji.roshenreji@gmail.com>
2021-02-04 10:32:56 +0530

    first commit

---
+++ mywebapp/src/main/java/com/roshen/mavenweb/MyClass.java
@@ -0,0 +1,13 @@
+package com.roshen.mavenweb;
+
+import java.util.*;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		List<String> names=new ArrayList<String>();
+
+	}
+
+}
